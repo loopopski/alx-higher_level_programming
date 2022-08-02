3607afe60107fce2c8112c742f4a05b66232f8d3
Same Titan <49342907+loopopski@users.noreply.github.com>
2022-08-01 21:22:29 -0600

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/103-fast_alphabet.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# Author: Martins Akhivbareme
-
-import string
-print(string.ascii_uppercase)
--- 0x02-python-import_modules/variable_load_5.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-# Author: Martins Akhivbareme
-
-a = 98
-"""Simple variable
-"""
