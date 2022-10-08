ad02bd8e2f42745a30fe0083fae7bdec87195975
Same Titan <49342907+loopopski@users.noreply.github.com>
2022-10-08 06:57:58 -0600

    Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.
+#!/usr/bin/python3
 """
 changes the name of a State object from the database hbtn_0e_6_usa
 """
@@ -11,7 +11,7 @@ from model_state import State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1],sys.argv[2],sys.argv[3]),
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     session_maker = sessionmaker(bind=engine)
     session = session_maker()
